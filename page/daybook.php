63516e5151eb550043366eb8753ad511e00d52dc
vijay mali <guddavijay@gmail.com>
2016-04-02 19:06:31 +0530

    day book filter  condition not cash account

--- page/daybook.php
+++ page/daybook.php
@@ -9,31 +9,40 @@ class page_daybook extends \Page{
 		$form->addField('DatePicker','date')->validateNotNull();
 		$form->addSubmit('Open Day Book');
 
-		$day_transaction_model = $this->add('xepan\accounts\Model_Transaction');
-		$transaction_row=$day_transaction_model->leftjoin('account_transaction_row.transaction_id');
-		$transaction_row->hasOne('xepan\accounts\Ledger','ledger_id');
-		$transaction_row->addField('amountDr');
-		$transaction_row->addField('amountCr');
-		
-		
-		$daybook_lister_crud = $this->add('xepan\hr\CRUD',['grid_class'=>'xepan\accounts\Grid_DayBook']);
+		$daybook_crud = $this->add('xepan\hr\CRUD',['grid_class'=>'xepan\accounts\Grid_DayBook']);
+		// $grid = $this->add('xepan\accounts\Grid_AccountsBase');
 
-		if($this->api->stickyGET('date_selected')){
-			$day_transaction_model->addCondition('created_at','>=',$_GET['date_selected']);
-			$day_transaction_model->addCondition('created_at','<',$this->api->nextDate($_GET['date_selected']));
-		}else{
-			$day_transaction_model->addCondition('created_at','>=',$this->api->today);
-			$day_transaction_model->addCondition('created_at','<',$this->api->nextDate($this->api->today));
-
+		$transaction_row = $this->add('xepan\accounts\Model_TransactionRow');
+		$group=$this->add('xepan\accounts\Model_Group')->loadCashAccount();
+		
+		$transaction_row->addCondition('root_group_name','<>',$group['name']);
+		
+		if($_GET['date_selected']){
+			$transaction_row->addCondition('created_at','>=',$_GET['date_selected']);
+			$transaction_row->addCondition('created_at','<',$this->api->nextDate($_GET['date_selected']));
+		}else{			
+			$transaction_row->addCondition('created_at','>=',$this->api->today);
+			$transaction_row->addCondition('created_at','<',$this->app->nextDate($this->api->today));
 		}
- 
-		$daybook_lister_crud->setModel($day_transaction_model,['voucher_no','transaction_type','Narration','ledger','amountDr','amountCr']);
-		$daybook_lister_crud->grid->removeColumn('Narration');
-		$daybook_lister_crud->grid->removeColumn('transaction_type');
+		$grid=$daybook_crud->grid;
+		
+		$grid->setModel($transaction_row,['voucher_no','transaction_type','created_at','Narration','account','amountDr','amountCr','root_group_name']);
+		// $grid->addSno();
+		$grid->removeColumn('account');
 
+		$grid->addMethod('format_transaction_type',function($g,$f){
+			if($g->model->transaction()->customer()){
+				$g->current_row_html[$f]=$g->model['transaction_type']."::".$g->model->transaction()->customer()->get('organization_name');
+			}else
+				$g->current_row_html[$f]=$g->model['transaction_type'];
+		});
+		$grid->addFormatter('transaction_type','transaction_type');
+
+		// $grid->addTotals(array('amountCr','amountDr'));
 		if($form->isSubmitted()){
-			$daybook_lister_crud->js()->reload(['date_selected'=>$form['date']?:0])->execute();
+			$grid->js()->reload(['date_selected'=>$form['date']?:0])->execute();
 		}
+
 
 	}
 }
