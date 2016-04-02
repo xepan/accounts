362f78be1c6e0c0cacd7c35e75243b18d125aa1a
sinharakesh <rksinha.btech@gmail.com>
2016-04-02 19:12:51 +0530

    removing transaction invoice association from accounts appliaction

--- page/amtreceived.php
+++ page/amtreceived.php
@@ -7,39 +7,39 @@ class page_amtreceived extends \Page {
 
 		// // ==== Cash PAYMENT ===========
 
-		// $received_from_model = $this->add('xepan\accounts\Model_Ledger');
+		$received_from_model = $this->add('xepan\accounts\Model_Ledger');
 
-		// $cash_accounts = $this->add('xepan\accounts\Model_Ledger')->loadCashAccounts();
+		$cash_accounts = $this->add('xepan\accounts\Model_Ledger')->loadCashAccounts();
 
-		// $form = $this->add('Form_Stacked',null,'cash_view');
-		// $form->setLayout('view/form/payment-received-cash');
+		$form = $this->add('Form_Stacked',null,'cash_view');
+		$form->setLayout('view/form/payment-received-cash');
 
-		// $form->addField('DatePicker','date')->set($this->api->now)->validateNotNull(true);
-		// $cash_field = $form->addField('autocomplete/Basic','cash_account')->validateNotNull(true);
-		// $cash_field->setModel($cash_accounts);
+		$form->addField('DatePicker','date')->set($this->api->now)->validateNotNull(true);
+		$cash_field = $form->addField('autocomplete/Basic','cash_account')->validateNotNull(true);
+		$cash_field->setModel($cash_accounts);
 
-		// $cash_field->set($this->add('xepan\accounts\Model_Ledger')->loadDefaultCashAccount()->get('id'));
+		$cash_field->set($this->add('xepan\accounts\Model_Ledger')->loadDefaultCashAccount()->get('id'));
 
-		// $received_from_field = $form->addField('autocomplete/Basic','received_from')->validateNotNull(true);
-		// $received_from_field->setModel($received_from_model);
+		$received_from_field = $form->addField('autocomplete/Basic','received_from')->validateNotNull(true);
+		$received_from_field->setModel($received_from_model);
 
-		// $form->addField('Money','amount')->validateNotNull(true);
-		// $form->addField('Text','narration');
-		// $form->addSubmit('Receive Now');
+		$form->addField('Money','amount')->validateNotNull(true);
+		$form->addField('Text','narration');
+		$form->addSubmit('Receive Now');
 
-		// if($form->isSubmitted()){
+		if($form->isSubmitted()){
 
-		// 	$transaction = $this->add('xepan\accounts\Model_Transaction');
-		// 	$transaction->createNewTransaction('CASH RECEIPT', null, $form['date'], $form['narration']);
+			$transaction = $this->add('xepan\accounts\Model_Transaction');
+			$transaction->createNewTransaction('CASH RECEIPT', null, $form['date'], $form['narration']);
 
-		// 	$transaction->addDebitAccount($this->add('xepan\accounts\Model_Ledger')->load($form['cash_account']),$form['amount']);
+			$transaction->addDebitAccount($this->add('xepan\accounts\Model_Ledger')->load($form['cash_account']),$form['amount']);
 			
-		// 	$transaction->addCreditAccount($this->add('xepan\accounts\Model_Ledger')->load($form['received_from']),$form['amount']);
+			$transaction->addCreditAccount($this->add('xepan\accounts\Model_Ledger')->load($form['received_from']),$form['amount']);
 
-		// 	$transaction->execute();
+			$transaction->execute();
 			
-		// 	$form->js(null,$form->js()->reload())->univ()->successMessage('Done')->execute();
-		// }
+			$form->js(null,$form->js()->reload())->univ()->successMessage('Done')->execute();
+		}
 
 
 
@@ -60,47 +60,6 @@ class page_amtreceived extends \Page {
 		$from_curreny_field->setModel('xepan\accounts\Currency');
 		$from_curreny_field->set($this->app->epan->default_currency->id);
 		$form->addField('line','from_exchange_rate')->validateNotNull(true);
-		
-		/*Invoices*/
-		$invoice_1_field = $form->addField('Dropdown','invoice_no_1')->validateNotNull(true);
-		$invoice_1_field->setModel('xepan\commerce\SalesInvoice');
-		$invoice_1_amount = $form->addField('line','invoice_amount_1')->validateNotNull(true);
-		$invoice_1_adjust = $form->addField('checkbox','invoice_adjust_1')->set(true);
-		$invoice_1_currency = $form->addField('Dropdown','invoice_currency_1')->validateNotNull(true);
-		$invoice_1_currency->setModel('xepan\accounts\Currency');
-		$invoice_1_exchange_rate = $form->addField('line','invoice_exchange_rate_1');
-
-		$invoice_2_field = $form->addField('Dropdown','invoice_no_2')->validateNotNull(true);
-		$invoice_2_field->setModel('xepan\commerce\SalesInvoice');
-		$invoice_2_amount = $form->addField('line','invoice_amount_2')->validateNotNull(true);
-		$invoice_2_adjust = $form->addField('checkbox','invoice_adjust_2')->set(true);
-		$invoice_2_currency = $form->addField('Dropdown','invoice_currency_2')->validateNotNull(true);
-		$invoice_2_currency->setModel('xepan\accounts\Currency');
-		$invoice_2_exchange_rate = $form->addField('line','invoice_exchange_rate_2');
-
-		$invoice_3_field = $form->addField('Dropdown','invoice_no_3')->validateNotNull(true);
-		$invoice_3_field->setModel('xepan\commerce\SalesInvoice');
-		$invoice_3_amount = $form->addField('line','invoice_amount_3')->validateNotNull(true);
-		$invoice_3_adjust = $form->addField('checkbox','invoice_adjust_3')->set(true);
-		$invoice_3_currency = $form->addField('Dropdown','invoice_currency_3')->validateNotNull(true);
-		$invoice_3_currency->setModel('xepan\accounts\Currency');
-		$invoice_3_exchange_rate = $form->addField('line','invoice_exchange_rate_3');
-
-		$invoice_4_field = $form->addField('Dropdown','invoice_no_4')->validateNotNull(true);
-		$invoice_4_field->setModel('xepan\commerce\SalesInvoice');
-		$invoice_4_amount = $form->addField('line','invoice_amount_4')->validateNotNull(true);
-		$invoice_4_adjust = $form->addField('checkbox','invoice_adjust_4')->set(true);
-		$invoice_4_currency = $form->addField('Dropdown','invoice_currency_4')->validateNotNull(true);
-		$invoice_4_currency->setModel('xepan\accounts\Currency');
-		$invoice_4_exchange_rate = $form->addField('line','invoice_exchange_rate_4');
-
-		$invoice_5_field = $form->addField('Dropdown','invoice_no_5')->validateNotNull(true);
-		$invoice_5_field->setModel('xepan\commerce\SalesInvoice');
-		$invoice_5_amount = $form->addField('line','invoice_amount_5')->validateNotNull(true);
-		$invoice_5_adjust = $form->addField('checkbox','invoice_adjust_5')->set(true);
-		$invoice_5_currency = $form->addField('Dropdown','invoice_currency_5')->validateNotNull(true);
-		$invoice_5_currency->setModel('xepan\accounts\Currency');
-		$invoice_5_exchange_rate = $form->addField('line','invoice_exchange_rate_5');
 
 
 		$bank_accounts = $this->add('xepan\accounts\Model_Ledger')->loadBankAccounts();
@@ -172,17 +131,19 @@ class page_amtreceived extends \Page {
 			//1// amount with currency and exchange rate
 				// CR (customer amount deposite the money) transaction
 			//2//
-				//Dr in Default Bank Account with 
+				//Dr in To Bank Account with 
 				//Dr in Bank Other change 1 
 				//Dr in Bank Other change 2 
 				//Dr in Bank Other change 3 
 
-			//3//
+			// 3 or 4 not come here because accounts don't know invoices
+			//3// it's just for managing how many and which one invoice are paid using this payment received transaction using invoive transaction association we calculate the you are in profit or loss
 				//marke one or more invoice complete
 				//invoice currency must be same as currency invoice send
 				//each invoice has check box about adjust profit/loss amount
 			//4//
 				//association entry in invoice_transaction_association_table
+
 
 			$transaction = $this->add('xepan\accounts\Model_Transaction');
 			$transaction->createNewTransaction('BANK RECEIPT', $related_document=false, $form['date'], $form['narration'], $Currency=null, $exchange_rate=1.00);
@@ -192,14 +153,14 @@ class page_amtreceived extends \Page {
 			$from_currency = $this->add('xepan\accounts\Model_Currency')->load($form['from_currency']);
 			
 			$transaction->addDebitAccount($from_ledger,$form['from_amount'],$from_currency,$form['from_exchange_rate']);
-			echo "DR From Account: ".$from_ledger->id." :amount= ".$form['from_amount']." :Currency= ".$from_currency->id." :exchange Rate=".$form['from_exchange_rate']."<br/>";
+			// echo "DR From Account: ".$from_ledger->id." :amount= ".$form['from_amount']." :Currency= ".$from_currency->id." :exchange Rate=".$form['from_exchange_rate']."<br/>";
 
 			//one entry for to bank 
 			$to_bank_ledger = $this->add('xepan\accounts\Model_Ledger')->load($form['to_bank_account']);
 			$to_bank_currency = $this->add('xepan\accounts\Model_Currency')->load($form['to_currency']);
 
 			$transaction->addCreditAccount($to_bank_ledger,$form['to_amount'],$to_bank_currency,$form['to_exchange_rate']);
-			echo "CR To Bank : ".$to_bank_ledger['name']." :amount= ".$form['to_amount']." :Currency= ".$to_bank_currency['name']." :exchange Rate=".$form['to_exchange_rate']."<br/>";
+			// echo "CR To Bank : ".$to_bank_ledger['name']." :amount= ".$form['to_amount']." :Currency= ".$to_bank_currency['name']." :exchange Rate=".$form['to_exchange_rate']."<br/>";
 
 			//entry for to bank other charge
 			for ($i=1; $i < 6; $i++) {
@@ -218,7 +179,7 @@ class page_amtreceived extends \Page {
 				$bank_other_charge_currency = $this->add('xepan\accounts\Model_Currency')->load($form[$currency_field]);
 
 				$transaction->addCreditAccount($bank_other_charge_ledger,$form[$amount_field],$bank_other_charge_currency,$form[$exchange_field]);
-				echo "CR To Bank Other Charge: ".$bank_other_charge_ledger['name']." :amount= ".$form[$amount_field]." :Currency= ".$bank_other_charge_currency['name']." :exchange Rate=".$form[$exchange_field]."<br/>";
+				// echo "CR To Bank Other Charge: ".$bank_other_charge_ledger['name']." :amount= ".$form[$amount_field]." :Currency= ".$bank_other_charge_currency['name']." :exchange Rate=".$form[$exchange_field]."<br/>";
 
 			}
 			
