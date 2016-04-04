dec63089bcd2ec990243d7e40eeeadd187668f38
vijay mali <guddavijay@gmail.com>
2016-04-04 11:42:50 +0530

    remove extra code

--- page/ledgers.php
+++ page/ledgers.php
@@ -5,23 +5,12 @@ class page_ledgers extends \Page{
 	function init(){
 		parent::init();
 
-		$group = $this->add('xepan\accounts\Model_Ledger');
+		$ledger = $this->add('xepan\accounts\Model_Ledger');
 		$crud = $this->add('xepan\hr\CRUD',null,null,['view/ledger-grid']);
-		$crud->setModel($group,['name','group_id','OpeningBalanceCr','OpeningBalanceDr'],['name','group','parent_group','root_group','CurrentBalanceCr','CurrentBalanceDr','OpeningBalanceCr','OpeningBalanceDr']);
+		$crud->setModel($ledger,['name','group_id','OpeningBalanceCr','OpeningBalanceDr'],['name','group','parent_group','root_group','CurrentBalanceCr','CurrentBalanceDr','OpeningBalanceCr','OpeningBalanceDr','balance']);
 
 		if(!$crud->isEditing()){
-			$crud->grid->addMethod('format_balance',function($g,$f){
-				$m=$g->model;
-
-				$fig = ($m['OpeningBalanceDr'] + $m['CurrentBalanceDr'])-($m['OpeningBalanceCr'] + $m['CurrentBalanceCr']);
-				
-				if($fig < 0)
-					$g->current_row_html[$f] = '<div class="pull-left">'.abs($fig) . '</div><div class="pull-right">Dr</div>';
-				else
-					$g->current_row_html[$f] = '<div class="pull-left">'.abs($fig) . '</div><div class="pull-right">Cr</div>';
-			});
-			$group->setOrder('name','asc');
-			$crud->grid->addColumn('balance','balance');
+			$ledger->setOrder('name','asc');
 			// $crud->grid->add_sno();
 			$crud->grid->addQuickSearch('name');
 			$crud->grid->removeColumn('CurrentBalanceDr');
