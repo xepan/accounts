8b00d0deca4e3a7d9ad4824d8051822c1fd887f8
dkanojia <dkanojia93@gmail.com>
2016-04-14 20:06:48 +0530

    ledger template added

--- lib/Initiator.php
+++ lib/Initiator.php
@@ -46,13 +46,16 @@ class Initiator extends \Controller_Addon {
 
 	function addAccountTemplates(){
 		$data =[
-					'customer'=>['name'=>'Customer', 'description'=>'Entries related to customer','group_id'=>37,'ledger_type'=>'SundryDebtor'],
-					'supplier' => ['name'=>'Supplier', 'description'=>'Entries related to Supplier','group_id'=>38,'ledger_type'=>'SundryCreditor'],
-					'outsourceparty' => ['name'=>'OutsourceParty', 'description'=>'Entries related to OutsourceParty','group_id'=>9,'ledger_type'=>'outsourceparty'],
+					'Customer'=>['name'=>'Customer', 'description'=>'Entries related to customer','group_id'=>$this->add('xepan\accounts\Model_Group')->loadSundryDebtor()->get('id'),'ledger_type'=>'Customer'],
+					'Supplier' => ['name'=>'Supplier', 'description'=>'Entries related to Supplier','group_id'=>$this->add('xepan\accounts\Model_Group')->loadSundryCreditor()->get('id'),'ledger_type'=>'Supplier'],
+					'outsourceparty' => ['name'=>'Outsource Party', 'description'=>'Entries related to OutsourceParty','group_id'=>$this->add('xepan\accounts\Model_Group')->loadSundryCreditor()->get('id'),'ledger_type'=>'OutsourceParty'],
 					'BankCharges' => ['name'=>'Bank Charges', 'description'=>'Any sort of bank chanrges','group_id'=>$this->add('xepan\accounts\Model_Group')->loadIndirectExpenses()->get('id'),'ledger_type'=>'BankCharges'],
-					'dutiesandtaxes' => ['name'=>'DutiesAndTaxes', 'description'=>'Entries related to DutiesAndTaxes','group_id'=>0,'ledger_type'=>'dutiesandtaxes'],
-					'directexpenses' => ['name'=>'DirectExpenses', 'description'=>'Entries related to Expenses','group_id'=>32,'ledger_type'=>'DirectExpenses'],
-					'indirectincome' => ['name'=>'IndirectIncome', 'description'=>'Entries related to Income','group_id'=>33,'ledger_type'=>'IndirectIncome'],
+					'DutiesAndTaxes' => ['name'=>'Duties And Taxes', 'description'=>'Entries related to DutiesAndTaxes','group_id'=>$this->add('xepan\accounts\Model_Group')->loadDutiesAndTaxes()->get('id'),'ledger_type'=>'DutiesAndTaxes'],
+					// 'DirectExpenses' => ['name'=>'Direct Expenses', 'description'=>'Entries related to Expenses','group_id'=>$this->add('xepan\accounts\Model_Group')->loadDirectExpenses->get('id'),'ledger_type'=>'DirectExpenses'],
+					'IndirectIncome' => ['name'=>'Indirect Income', 'description'=>'Entries related to Income','group_id'=>$this->add('xepan\accounts\Model_Group')->loadIndirectIncome()->get('id'),'ledger_type'=>'IndirectIncome'],
+					'SuspenseLedger' => ['name'=>'Suspense', 'description'=>'Entries related to Income','group_id'=>$this->add('xepan\accounts\Model_Group')->loadSuspenseLedger()->get('id'),'ledger_type'=>'Suspense'],
+					'SecuredLoan' => ['name'=>'Loan', 'description'=>'Entries related to Income','group_id'=>$this->add('xepan\accounts\Model_Group')->loadSecuredLoan()->get('id'),'ledger_type'=>'Loan'],
+					'FixedAssets' => ['name'=>'Furniture', 'description'=>'Entries related to Income','group_id'=>$this->add('xepan\accounts\Model_Group')->loadFixedAssets()->get('id'),'ledger_type'=>'FixedAssets']
 					
 		];
 		$this->app->setConfig('account_template_data',$data);
