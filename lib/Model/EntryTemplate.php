7a4c3922ddf6c5f3a4adc8ae152efdd87d41000c
vijay mali <guddavijay@gmail.com>
2016-08-03 15:00:13 +0530

    entertemplate transaction executor form layout multiple template layout created

--- lib/Model/EntryTemplate.php
+++ lib/Model/EntryTemplate.php
@@ -20,12 +20,54 @@ class Model_EntryTemplate extends \xepan\base\Model_Table{
 	function manageForm($page, $related_id=null, $related_type=null){
 		$transactions = $this->ref('xepan\accounts\EntryTemplateTransaction');
 
-		$form = $page->add('xepan\accounts\Form_EntryRunner',null,null,['form/empty']);
-		$form->setLayout(['view/form/accounttransactionexecuter']);
+		$template = $this->add('GiTemplate');
+		$template->loadTemplate('view/form/entrytransaction');
+		$template->trySetHTML('date','{$date}');
+		
+
+		foreach ($transactions as $trans) {
+			$transaction_template = $this->add('GiTemplate');
+			$transaction_template->loadTemplate('view/form/entrytransactionsides');
+
+			$transaction_template->trySetHTML('transaction_name','{$transaction_name_'.$trans->id.'}');
+
+			foreach ($trans->ref('xepan\accounts\EntryTemplateTransactionRow') as $row) {
+				if($row['side']=="Dr"){
+					$row_left_template = $this->add('GiTemplate');
+					$row_left_template->loadTemplate('view/form/entrytransactionsiderows');
+					$row_left_template->trySetHTML('ledger','{$left_ledger_'.$row->id.'}');
+					$row_left_template->trySetHTML('amount','{$left_amount_'.$row->id.'}');
+					$row_left_template->trySetHTML('currency','{$left_currency_'.$row->id.'}');
+					$row_left_template->trySetHTML('exchange_rate','{$left_exchange_rate_'.$row->id.'}');
+					$transaction_template->appendHTML('transaction_row_left',$row_left_template->render());
+					
+				}else{
+					$row_right_template = $this->add('GiTemplate');
+					$row_right_template->loadTemplate('view/form/entrytransactionsiderows');
+					$row_right_template->trySetHTML('ledger','{$right_ledger_'.$row->id.'}');
+					$row_right_template->trySetHTML('amount','{$right_amount_'.$row->id.'}');
+					$row_right_template->trySetHTML('currency','{$right_currency_'.$row->id.'}');
+					$row_right_template->trySetHTML('exchange_rate','{$right_exchange_rate_'.$row->id.'}');
+					$transaction_template->appendHTML('transaction_row_right',$row_right_template->render());
+				}	
+				
+			}
+			$template->appendHTML('transactions',$transaction_template->render());
+
+		}
+
+		// echo (htmlentities($template->render()));
+		// exit;
+
+		$template->loadTemplateFromString($template->render());
+
+		$form = $page->add('xepan\accounts\Form_EntryRunner',null,null,['form/stacked']);
+		$form->setLayout($template);
 
 		$form->addField('DatePicker','date');
 		
 		foreach ($transactions as $trans) {
+			$form->layout->add('View',null,'transaction_name_'.$trans->id)->set($trans['name']);
 			foreach ($trans->ref('xepan\accounts\EntryTemplateTransactionRow') as $row) {
 
 				if($row['is_allow_add_ledger'])
@@ -35,7 +77,7 @@ class Model_EntryTemplate extends \xepan\base\Model_Table{
 				
 				$spot = $row['side']=='Dr'?'left':'right';			
 
-				$field = $form->addField($field_type,'ledger_'.$row->id, $row['title'],null,$spot);
+				$field = $form->addField($field_type,'ledger_'.$row->id, $row['title'],null,$spot.'_ledger_'.$row->id);
 				$field->show_fields= ['name'];
 
 				$row_ledger_present = $row['ledger']?true:false;
@@ -68,11 +110,11 @@ class Model_EntryTemplate extends \xepan\base\Model_Table{
 
 				$field->setModel($ledger);
 				if($row['is_include_currency']){
-					$form_currency = $form->addField('Dropdown','bank_currency_'.$row->id,'Currency Name',null,$spot."_currency");
+					$form_currency = $form->addField('Dropdown','bank_currency_'.$row->id,'Currency Name',null,$spot.'_currency_'.$row->id);
 					$form_currency->setModel('xepan\accounts\Currency');
-					$exchange_rate = $form->addField('line','to_exchange_rate'.$row->id,'Currency Rate',null,$spot."_exchange_rate")->validateNotNull(true)->addClass('exchange-rate');
+					$exchange_rate = $form->addField('line','to_exchange_rate'.$row->id,'Currency Rate',null,$spot.'_exchange_rate_'.$row->id)->validateNotNull(true)->addClass('exchange-rate');
 				}
-				$form->addField('line','amount_'.$row->id,'Amount',null,$spot."_amount");
+				$field = $form->addField('line','amount_'.$row->id,'Amount',null,$spot.'_amount_'.$row->id);
 			}
 		}
 
@@ -82,7 +124,7 @@ class Model_EntryTemplate extends \xepan\base\Model_Table{
 			foreach ($transactions as $trans) {
 				$transaction = $this->add('xepan\accounts\Model_Transaction');
 				if($form_currency){
-					$currency = $this->add('xepan\accounts\Model_Currency')->load($form['bank_currency_'.$row->id]);
+					$currency = $this->add('xepan\accounts\Model_Currency')->tryLoad($form['bank_currency_'.$row->id]);
 					$transaction->createNewTransaction($trans['type'], null, $form['date'], $form['narration'],$currency,$exchange_rate);
 				}else{
 					$transaction->createNewTransaction($trans['type'], null, $form['date'], $form['narration']);
