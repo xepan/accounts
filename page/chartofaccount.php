b75cbfdaca955d5e4a44e17b4b140c792ae91ee8
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-03 18:37:38 +0530

    Chart of account .. working ok.. todo.. check completely

--- page/chartofaccount.php
+++ page/chartofaccount.php
@@ -13,50 +13,57 @@ class page_chartofaccount extends \xepan\base\Page{
 		$obj = [];
 
 		foreach ($balance_model as $bm) {
-			$obj['name'] = $bm['name'];
+			$data = ['name' => $bm['name']];
 			foreach ($bm->ref('xepan\accounts\Group')->addCondition('parent_group_id',null) as $root_groups) {
-				if(!isset($obj['groups'])) $obj['groups']=[];
 				$group_data=['name'=>$root_groups['name']];
 				$group_data['groups'] = $this->getChildGroups($root_groups);
 				$group_data['ledgers']=[];
 				foreach ($root_groups->ref('xepan\accounts\Ledger') as $ledger) {
-					$group_data['ledgers'][]= $ledger['name'];
+					$group_data['ledgers'][]= ['name'=>$ledger['name']];
 				}
-				$obj['groups'][] = $group_data;
+				$data['groups'][] = $group_data;
 			}
+			$obj[] = $data;
 		}
 
-		echo "<pre>";
+		// echo "<pre>";
+		// var_dump($obj);
+		// exit;
 
-		var_dump($obj);
-		exit;
-
-		// $this->add('View')->set($this->make_ulli([$obj]));
+		$this->add('View')->setHTML($this->make_ulli($obj));
 	}
 
 	function getChildGroups($group){
-		$t= [];
+		$data= [];
 		foreach ($this->add('xepan\accounts\Model_Group')->addCondition('parent_group_id',$group->id) as $sg) {
 			$t=['name'=>$sg['name']];
 			$t['groups'] = $this->getChildGroups($sg);
 			$t['ledgers']=[];
 			foreach ($sg->ref('xepan\accounts\Ledger') as $ledger) {
-				$t['ledgers'][]= $ledger['name'];
+				$t['ledgers'][]= ['name'=>$ledger['name']];
 			}
+			$data = $t;
 		}
-		return $t;
+		return $data;
 	}
 
 	function make_ulli($array){
 	    if(!is_array($array)) return '';
+	    // echo "working on ";
+	    // var_dump($array);
 
 	    $output = '<ul>';
 	    foreach($array as $item){  
 
-	        $output .= '<li>' . $item->name;      
+	        $output .= '<li>' . $item['name'];
+	        // echo $item['name']. ' -- ';
+	        if(isset($item['groups']) && count($item['groups'])){
+	        	// var_dump($item['groups']);
+	            $output .= $this->make_ulli($item['groups']);
+	        }
 
-	        if(property_exists($item, 'childs'))
-	            $output .= $this->make_ulli($item->childs);
+	        if(isset($item['ledgers']) && count($item['ledgers']))
+	            $output .= $this->make_ulli($item['ledgers']);
 
 	        $output .= '</li>';
 
