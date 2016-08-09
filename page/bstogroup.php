0fb31b30cdd135b137cc38b96e8543becd223f8a
vaibhav <vaibhavsharma38@gmail.com>
2016-08-09 12:59:51 +0530

    working on bstogroup page

--- page/bstogroup.php
+++ page/bstogroup.php
@@ -6,6 +6,11 @@ class page_bstogroup extends \xepan\base\Page{
 	public $title = "Balance Sheet Groups";
 	function init(){
 		parent::init();
-	
+		
+		$bs_id = $this->api->stickyGET('bs_id');
+		$bs_group = $this->add('xepan\accounts\Model_BSGroup');
+		$bs_group->addCondititon('balance_sheet_id',$bs_id);
+
+		$this->add('grid')->setModel($bs_group);
 	}
 }
