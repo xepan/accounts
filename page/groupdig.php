cea6843eab37e91607dd7991d9fd9b9ca0c9f6d5
vaibhav <vaibhavsharma38@gmail.com>
2016-08-09 19:47:09 +0530

    amount total done

--- page/groupdig.php
+++ page/groupdig.php
@@ -15,6 +15,14 @@ class page_groupdig extends \xepan\base\Page{
 		$bs_group = $this->add('xepan\accounts\Model_BSGroup');
 		$bs_group->addCondition('parent_group_id',$group_id);
 
+		$dr_bal = 0;
+		$cr_bal = 0;
+
+		foreach ($bs_group as $bsg) {
+			$dr_bal += $bsg['ClosingBalanceDr']; 
+			$cr_bal += $bsg['ClosingBalanceCr'];
+		}
+
 		$subgroupandledger = [];
 		foreach ($bs_group as $group){
 			$subgroupandledger[] = ['name'=>$group['name'],'type'=>'group','id'=>$group['id'],'class'=>'xepan-accounts-sub-group','balancecr'=>$group['ClosingBalanceCr'],'balancedr'=>$group['ClosingBalanceDr']]; 
@@ -22,6 +30,11 @@ class page_groupdig extends \xepan\base\Page{
 		
 		$bs_ledger = $this->add('xepan\accounts\Model_BSLedger');
 		$bs_ledger->addCondition('group_id',$group_id);
+
+		foreach ($bs_ledger as $led) {
+			$dr_bal += $led['ClosingBalanceDr']; 
+			$cr_bal += $led['ClosingBalanceCr'];
+		}
 
 		foreach ($bs_ledger as $ledger){
 			$subgroupandledger[] = ['name'=>$ledger['name'],'type'=>'ledger','id'=>$ledger['id'],'class'=>'xepan-accounts-ledger','balancecr'=>$ledger['ClosingBalanceCr'],'balancedr'=>$ledger['ClosingBalanceDr']]; 
@@ -35,6 +48,8 @@ class page_groupdig extends \xepan\base\Page{
 		$grid->template->trySet('parent',$g['name']);
 		$grid->template->trySet('from_date',$from_date);
 		$grid->template->trySet('to_date',$to_date);
+		$grid->template->trySet('dr',$dr_bal);
+		$grid->template->trySet('cr',$cr_bal);
 
         $this->js('click')->_selector('.xepan-accounts-ledger')->univ()->frameURL('Account Statement',[$this->api->url('xepan_accounts_statement'),'ledger_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id'), 'from_date'=>$from_date, 'to_date'=>$to_date]);	
         $this->js('click')->_selector('.xepan-accounts-sub-group')->univ()->frameURL('Groups And Ledger',[$this->api->url('xepan_accounts_groupdig'),'group_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id'), 'from_date'=>$from_date, 'to_date'=>$to_date]);	
