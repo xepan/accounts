a0606c6d8028469f3a98dcb086a93ea6f79d3b73
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-09 13:10:50 +0530

    BSLedger  updated

--- lib/Model/BSLedger.php
+++ lib/Model/BSLedger.php
@@ -14,54 +14,47 @@ class Model_BSLedger extends Model_Ledger {
 	function init(){
 		parent::init();
 
-		$this->addExpression('PreviousTransactionDR')->set(function($m,$q){
-			return $m->refSQL('TransactionRows')->addCondition('created_at','<',$this->from_date)->sum('amountDr');
+		$this->addExpression('PreviousTransactionsDr')->set(function($m,$q){
+			$transaction =  $m->add('xepan\accounts\Model_TransactionRow');
+			return $transaction->addCondition('ledger_id',$q->getField('id'))
+								->addCondition('created_at','<',$this->from_date)
+								->sum($q->expr('IFNULL([0],0)',[$transaction->getElement('amountDr')]));
+		});
+		$this->addExpression('PreviousTransactionsCr')->set(function($m,$q){
+			$transaction =  $m->add('xepan\accounts\Model_TransactionRow');
+			return $transaction->addCondition('ledger_id',$q->getField('id'))
+								->addCondition('created_at','<',$this->from_date)
+								->sum($q->expr('IFNULL([0],0)',[$transaction->getElement('amountCr')]));
 		});
 
-		$this->addExpression('PreviousTransactionCR')->set(function($m,$q){
-			return $m->refSQL('TransactionRows')->addCondition('created_at','<',$this->from_date)->sum('amountCr');
+		$this->addExpression('TransactionsDr')->set(function($m,$q){
+			$transaction =  $m->add('xepan\accounts\Model_TransactionRow');
+			return $transaction->addCondition('ledger_id',$q->getField('id'))
+								->addCondition('created_at','>=',$this->from_date)
+								->addCondition('created_at','<',$this->app->nextDate($this->from_date))
+								->sum($q->expr('IFNULL([0],0)',[$transaction->getElement('amountDr')]));
+		});
+		$this->addExpression('TransactionsCr')->set(function($m,$q){
+			$transaction =  $m->add('xepan\accounts\Model_TransactionRow');
+			return $transaction->addCondition('ledger_id',$q->getField('id'))
+								->addCondition('created_at','>=',$this->from_date)
+								->addCondition('created_at','<',$this->app->nextDate($this->from_date))
+								->sum($q->expr('IFNULL([0],0)',[$transaction->getElement('amountCr')]));
 		});
 
-		$this->addExpression('openBalanceDRSUM')->set(function($m,$q){
-			$m1 = $this->add('xepan\accounts\Model_BSLedger',['table_alias'=>'opdrsum']);
-			return $m1->addCondition('balance_sheet_id',$m->getElement('balance_sheet_id'))
-					->sum('OpeningBalanceDr');
+		$this->addExpression('ClosingBalanceDr')->set(function($m,$q){
+			return $q->expr('IFNULL([0],0)+IFNULL([1],0)+IFNULL([2],0)',[
+					$m->getElement('OpeningBalanceDr'),
+					$m->getElement('PreviousTransactionsDr'),
+					$m->getElement('TransactionsDr')
+				]);
 		});
-
-		$this->addExpression('openBalanceCRSUM')->set(function($m,$q){
-			$m1 = $this->add('xepan\accounts\Model_BSLedger',['table_alias'=>'opcrsum']);
-			return $m1->addCondition('balance_sheet_id',$m->getElement('balance_sheet_id'))
-					->sum('OpeningBalanceCr');
-		});
-
-		$this->addExpression('OpeningBalanceDrOnDate')->set(function($m,$q){
-			return $q->expr('IFNULL([0],0)+ IFNULL([1],0)',[$m->getElement('openBalanceDRSUM'),$m->getElement('PreviousTransactionDR')]);
-		});
-
-		$this->addExpression('OpeningBalanceCrOnDate')->set(function($m,$q){
-			return $q->expr('IFNULL([0],0)+IFNULL([1],0)',[$m->getElement('openBalanceCRSUM'),$m->getElement('PreviousTransactionCR')]);
-		});
-
-		$this->addExpression('transactionsDR')->set(function($m,$q){
-			return $m->refSQL('TransactionRows')
-							->addCondition('created_at','>=',$this->from_date)
-							->addCondition('created_at','<',$this->app->nextDate($this->to_date))
-							->sum('amountDr');
-		});
-
-		$this->addExpression('transactionsCR')->set(function($m,$q){
-			return $m->refSQL('TransactionRows')
-							->addCondition('created_at','>=',$this->from_date)
-							->addCondition('created_at','<',$this->app->nextDate($this->to_date))
-							->sum('amountCr');
-		});
-
-		$this->addExpression('closingBalanceDR')->set(function($m,$q){
-			return $q->expr('[0]+[1]',[$m->getElement('OpeningBalanceDrOnDate'),$m->getElement('transactionsDR')]);
-		});
-
-		$this->addExpression('closingBalanceCR')->set(function($m,$q){
-			return $q->expr('[0]+[1]',[$m->getElement('OpeningBalanceCrOnDate'),$m->getElement('transactionsCR')]);
+		$this->addExpression('ClosingBalanceCr')->set(function($m,$q){
+			return $q->expr('IFNULL([0],0)+IFNULL([1],0)+IFNULL([2],0)',[
+					$m->getElement('OpeningBalanceCr'),
+					$m->getElement('PreviousTransactionsCr'),
+					$m->getElement('TransactionsCr')
+				]);
 		});
 
 	}
