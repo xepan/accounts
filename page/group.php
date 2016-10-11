1a0b0f14e2140903887222906deca6a75b54749a
dkanojia <dkanojia93@gmail.com>
2016-10-11 16:28:49 +0530

    changes .............

--- page/group.php
+++ page/group.php
@@ -13,5 +13,6 @@ class page_group extends \xepan\base\Page{
 		//				['name','parent_group_id','balance_sheet_id','root_group_id','created_at'],
 		//				['name','parent_group','balance_sheet','root_group','created_at']);
 		$crud->grid->addPaginator(10);
+		$crud->grid->addQuickSearch(['name']);
 	}
 }
