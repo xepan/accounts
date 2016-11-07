15d89611404197426874462f401861664dc81242
dkanojia <dkanojia93@gmail.com>
2016-11-07 19:37:26 +0530

    form converted to read only mode

--- page/audit.php
+++ page/audit.php
@@ -27,11 +27,12 @@ class page_audit extends \xepan\base\Page {
 		$form->addField('CheckBox','update_in_account_transaction');
 		$form->addField('CheckBox','update_in_account_entry_template');
 		
-		$rem_ldgr_name = $form->addField('Line','ldgr_for_remove_ledger_name');
-		$rem_ldgr_type = $form->addField('Line','ldgr_for_remove_ledger_type');
-		$rem_ldgr_group = $form->addField('Line','ldgr_for_remove_group_name');
-		$rem_ldgr_op_bal_dr = $form->addField('Line','ldgr_for_remove_OpeningBalanceDr');
-		$rem_ldgr_op_bal_cr = $form->addField('Line','ldgr_for_remove_OpeningBalanceCr');
+		$rem_ldgr_name = $form->addField('Readonly','ldgr_for_remove_ledger_name');
+		$rem_ldgr_type = $form->addField('Readonly','ldgr_for_remove_ledger_type');
+		$rem_ldgr_group = $form->addField('Readonly','ldgr_for_remove_group_name');
+		$rem_ldgr_op_bal_dr = $form->addField('Readonly','ldgr_for_remove_OpeningBalanceDr');
+		$rem_ldgr_op_bal_cr = $form->addField('Readonly','ldgr_for_remove_OpeningBalanceCr');
+		$rem_ldgr_contact = $form->addField('Readonly','ldgr_for_remove_contact');
 
 		if($rem_ldgr_id){
 			$rem_ldgr_mdl = $this->add('xepan\accounts\Model_Ledger');
@@ -42,14 +43,16 @@ class page_audit extends \xepan\base\Page {
 			$rem_ldgr_group->set($rem_ldgr_mdl['group']);
 			$rem_ldgr_op_bal_dr->set($rem_ldgr_mdl['OpeningBalanceDr']);
 			$rem_ldgr_op_bal_cr->set($rem_ldgr_mdl['OpeningBalanceCr']);
+			$rem_ldgr_contact->set($rem_ldgr_mdl['contact_id']);
 		}
 
-		$new_ldgr_name = $form->addField('Line','new_merged_ldgr_ledger_name');
-		$rew_ldgr_type = $form->addField('Line','new_merged_ldgr_ledger_type');
-		$new_ldgr_group = $form->addField('Line','new_merged_ldgr_group_name');
-		$new_ldgr_op_bal_dr = $form->addField('Line','new_merged_ldgr_OpeningBalanceDr');
-		$new_ldgr_op_bal_cr = $form->addField('Line','new_merged_ldgr_OpeningBalanceCr');
-		
+		$new_ldgr_name = $form->addField('Readonly','new_merged_ldgr_ledger_name');
+		$rew_ldgr_type = $form->addField('Readonly','new_merged_ldgr_ledger_type');
+		$new_ldgr_group = $form->addField('Readonly','new_merged_ldgr_group_name');
+		$new_ldgr_op_bal_dr = $form->addField('Readonly','new_merged_ldgr_OpeningBalanceDr');
+		$new_ldgr_op_bal_cr = $form->addField('Readonly','new_merged_ldgr_OpeningBalanceCr');
+		$new_ldgr_contact = $form->addField('Readonly','new_merged_ldgr_contact');
+
 		if($new_ldgr_id){
 			$new_ldgr_mdl = $this->add('xepan\accounts\Model_Ledger');
 			$new_ldgr_mdl->load($new_ldgr_id);
@@ -59,10 +62,8 @@ class page_audit extends \xepan\base\Page {
 			$new_ldgr_group->set($new_ldgr_mdl['group']);
 			$new_ldgr_op_bal_dr->set($new_ldgr_mdl['OpeningBalanceDr']);
 			$new_ldgr_op_bal_cr->set($new_ldgr_mdl['OpeningBalanceCr']);
+			$new_ldgr_contact->set($new_ldgr_mdl['contact_id']);
 		}
-
-		// $this->addExpression('CurrentBalanceDr')->set(function($m,$q){
-		// $this->addExpression('CurrentBalanceCr')->set(function($m,$q){
 
 		$form->addSubmit('Merge Ledger')->addClass('btn btn-primary');
 
@@ -75,25 +76,9 @@ class page_audit extends \xepan\base\Page {
 			$removed_ledger_mdl->loadBy('id',$rem_ldgr_id);
 			// $removed_ledger_mdl->loadBy('id',$form['ldgr_for_remove']);
 
-			// $new_merged_ldgr_mdl['name'] = $removed_ledger_mdl['name'];
-			// $new_merged_ldgr_mdl['group_id'] = $removed_ledger_mdl['group_id'];
-			// $new_merged_ldgr_mdl['related_id'] = $removed_ledger_mdl['related_id'];
-			// $new_merged_ldgr_mdl['ledger_type'] = $removed_ledger_mdl['ledger_type'];
-			// $new_merged_ldgr_mdl['LedgerDisplayName'] = $removed_ledger_mdl['LedgerDisplayName'];
-			
-			// $customer = $this->add('xepan\commerce\Model_Customer');
-			// $customer->addCondition('contact_id',$removed_ledger_mdl['contact_id']);
-			// $customer->loadBy('organization_name',$new_merged_ldgr_mdl['name']);
-			
-			// $supplier = $this->add('xepan\commerce\Model_Supplier');
-			// $supplier->addCondition('contact_id',$removed_ledger_mdl['contact_id']);
-			// $supplier->loadBy('name',$new_merged_ldgr_mdl['name']);
+			if($removed_ledger_mdl['contact_id'])
+				$new_merged_ldgr_mdl['contact_id'] = $removed_ledger_mdl['contact_id'];
 
-			// if($supplier->loaded()){
-			// 	$new_merged_ldgr_mdl['contact_id'] = $customer['contact_id'];
-			// }
-			
-			$new_merged_ldgr_mdl['contact_id'] = $removed_ledger_mdl['contact_id'];
 			$new_merged_ldgr_mdl['OpeningBalanceCr'] += $removed_ledger_mdl['OpeningBalanceCr'];
 			$new_merged_ldgr_mdl['OpeningBalanceDr'] += $removed_ledger_mdl['OpeningBalanceDr'];
 			$new_merged_ldgr_mdl->save();
