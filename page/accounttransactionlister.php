96e2f3d05efc20c6f2b6b1fe62682a6397f6c14b
dkanojia <dkanojia93@gmail.com>
2016-11-16 13:21:28 +0530

    transaction lister updated

--- page/accounttransactionlister.php
+++ page/accounttransactionlister.php
@@ -7,13 +7,13 @@ class page_accounttransactionlister extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$grid = $this->add('xepan\hr\Grid',null,null,['view/grid/account-transaction-lister-run']);
+		$crud = $this->add('xepan\hr\CRUD',['allow_add'=>false],null,['view/grid/account-transaction-lister-run']);
 		$transaction_m = $this->add('xepan\accounts\Model_EntryTemplate');
 		$transaction_m->add('xepan\hr\Controller_ACL');
 		
-		$grid->setModel($transaction_m);
-		$grid->addQuickSearch(['name']);
-		$run_executer = $grid->addColumn('button','Run');
+		$crud->setModel($transaction_m);
+		$crud->grid->addQuickSearch(['name']);
+		$run_executer = $crud->grid->addColumn('button','Run');
 
 		if($_GET['Run']){
 			$this->app->redirect($this->app->url('xepan_accounts_accounttransactionexecuter',['accounts_template_id'=>$_GET['Run']]));
