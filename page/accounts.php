f19319ed0ac20e7c7f4c4150b8c19e6633fffb36
dkanojia <dkanojia93@gmail.com>
2016-11-28 18:07:37 +0530

    account app updated

--- page/accounts.php
+++ page/accounts.php
@@ -22,6 +22,7 @@ class page_accounts extends \xepan\base\Page
 		$crud->grid->addPaginator(10);
 		$frm=$crud->grid->addQuickSearch(['name']);
 		$crud->add('xepan\base\Controller_Avatar');
+		$crud->add('xepan\base\Controller_MultiDelete');
 
 		if(!$crud->isEditing()){
 			$crud->grid->js('click')->_selector('.do-view-leadger')->univ()->frameURL('Ledgers Details',[$this->api->url('xepan_accounts_statement'),'ledger_id'=>$this->js()->_selectorThis()->closest('[data-id]')->data('id')]);
