7f93d7f4d7cd48731add7889b15166144483f224
dkanojia <dkanojia93@gmail.com>
2017-02-14 11:17:30 +0530

    accounts report layout table added

--- page/report.php
+++ page/report.php
@@ -5,7 +5,8 @@ class page_report extends \xepan\base\Page{
 	function init(){
 		parent::init();
 
-		$this->app->side_menu->addItem([' Tax Report','icon'=>' fa fa-tax'],'xepan_accounts_report_subtax')->setAttr(['title'=>'Tax Report ']);		
-		$this->app->side_menu->addItem([' BalanceSheet Formatted','icon'=>' fa fa-tax'],'xepan_accounts_report_balancesheet')->setAttr(['title'=>'Tax Report ']);		
+		$this->app->side_menu->addItem([' Reports ','icon'=>'  fa fa-file-text-o'],'xepan_accounts_report_all')->setAttr(['title'=>'Reports ']);		
+		$this->app->side_menu->addItem([' Tax Report','icon'=>' fa fa-file-text-o'],'xepan_accounts_report_subtax')->setAttr(['title'=>'Tax Report ']);		
+		$this->app->side_menu->addItem([' BalanceSheet Formatted','icon'=>' fa fa-file-text-o'],'xepan_accounts_report_balancesheet')->setAttr(['title'=>'Tax Report ']);		
 	}
 }
