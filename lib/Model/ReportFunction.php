2a173cad56bad59c787870af4451bfb97ec3ef33
sinharakesh <rksinha.btech@gmail.com>
2017-02-18 17:43:53 +0530

    getResult function added

--- lib/Model/ReportFunction.php
+++ lib/Model/ReportFunction.php
@@ -25,12 +25,13 @@ class Model_ReportFunction extends \xepan\base\Model_Table{
 								'GroupTransactionSUMCR',
 								'GroupOnlyTransactionSUMDR',
 								'GroupOnlyTransactionSUMCR',
-								'PANDL'
+								'PANDL',
+								'Trading'
 							]);
 
-		$this->addField('group_value');
-		$this->addField('head_value')->type('text');
-		$this->addField('ledger_value')->type('text');
+		$this->hasOne('xepan\accounts\Group','group_id');
+		$this->hasOne('xepan\accounts\BalanceSheet','head_id');
+		$this->hasOne('xepan\accounts\Ledger','ledger_id');
 
 		$this->addField('start_date')->type('date');
 		$this->addField('end_date')->type('date');
@@ -47,7 +48,137 @@ class Model_ReportFunction extends \xepan\base\Model_Table{
 
 	function getResult(){
 		if(!$this->loaded()) throw new \Exception("layout model must loaded", 1);
-		return 10;		
+
+		switch ($this['type']) {
+		
+			case 'GroupBalance':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['group_id']);
+				if($model['subtract_from'] == "DR")
+					return $model['ClosingBalanceDr'] - $model['ClosingBalanceCr'];
+				else
+					return $model['ClosingBalanceCr'] - $model['ClosingBalanceDr'];
+			break;
+
+			case 'GroupOnlyBalance':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date'],'no_sub_groups'=>true]);
+				$model->load($this['group_id']);
+				if($model['subtract_from'] == "DR")
+					return $model['ClosingBalanceDr'] - $model['ClosingBalanceCr'];
+				else
+					return $model['ClosingBalanceCr'] - $model['ClosingBalanceDr'];
+			break;
+			
+			case 'GroupDR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['group_id']);
+				return $model['ClosingBalanceDr'];
+			break;
+
+			case 'GroupCR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['group_id']);
+				return $model['ClosingBalanceCr'];
+			break;
+
+			case 'GroupTransactionSUMDR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['group_id']);
+				return $model['TransactionsDr'];
+			break;
+
+			case 'GroupTransactionSUMCR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['group_id']);
+				return $model['TransactionsCr'];
+			break;
+
+			case 'GroupOnlyDR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date'],'no_sub_groups'=>true]);
+				$model->load($this['group_id']);
+				return $model['ClosingBalanceDr'];
+			break;
+
+			case 'GroupOnlyCR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date'],'no_sub_groups'=>true]);
+				$model->load($this['group_id']);
+				return $model['ClosingBalanceCr'];
+			break;
+
+
+			case 'GroupOnlyTransactionSUMDR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date'],'no_sub_groups'=>true]);
+				$model->load($this['group_id']);
+				return $model['TransactionsDr'];
+			break;
+
+			case 'GroupOnlyTransactionSUMCR':
+				$model = $this->add('xepan\accounts\Model_BSGroup',['from_date'=>$this['start_date'],'to_date'=>$this['end_date'],'no_sub_groups'=>true]);
+				$model->load($this['group_id']);
+				return $model['TransactionsCr'];
+			break;
+
+			case 'LedgerBalance':
+				$model = $this->add('xepan\accounts\Model_BSLedger',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->addExpression('subtract_from')->set(function($m,$q){
+					$bs_m = $m->add('xepan\accounts\Model_BalanceSheet');
+					return $bs_m->addCondition('id',$q->getField('balance_sheet_id'))
+							->fieldQuery($bs_m->getElement('subtract_from'));
+				});
+
+				$model->load($this['ledger_id']);
+				if($model['subtract_from'] == "DR")
+					return $model['ClosingBalanceDr'] - $model['ClosingBalanceCr'];
+				else
+					return $model['ClosingBalanceCr'] - $model['ClosingBalanceDr'];
+			break;
+
+			case 'HeadBalance':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['head_id']);
+				if($model['subtract_from'] == "DR")
+					return $model['ClosingBalanceDr'] - $model['ClosingBalanceCr'];
+				else
+					return $model['ClosingBalanceCr'] - $model['ClosingBalanceDr'];
+			break;
+
+			case 'HeadDR':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['head_id']);
+				return $model['ClosingBalanceDr'];
+			break;
+
+			case 'HeadCR':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['head_id']);
+				return $model['ClosingBalanceCr'];
+			break;
+
+			case 'HeadTransactionSUMDR':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['head_id']);
+				return $model['TransactionsDr'];
+			break;
+
+			case 'HeadTransactionSUMCR':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet',['from_date'=>$this['start_date'],'to_date'=>$this['end_date']]);
+				$model->load($this['head_id']);
+				return $model['TransactionsCr'];
+			break;
+
+			case 'PANDL':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet');
+				$result = $model->getPandL($this['start_date'],$this['end_date']);
+				return $result['net_profit'] - $result['net_loss'];
+			break;
+
+			case 'Trading':
+				$model = $this->add('xepan\accounts\Model_BSBalanceSheet');
+				$result = $model->getTradingBalance($this['start_date'],$this['end_date']);
+				return $result['gross_profit'] - $result['gross_loss'];
+			break;
+		}
+
 	}	
 }
 
