05ee2ecaa9b36c6c1fc7b381dabab90ef681c2da
sinharakesh <rksinha.btech@gmail.com>
2017-02-18 17:43:24 +0530

    suvbtract from Cr value added

--- lib/Model/BalanceSheet.php
+++ lib/Model/BalanceSheet.php
@@ -15,7 +15,7 @@ class Model_BalanceSheet extends \xepan\base\Model_Table{
 		$this->addField('positive_side')->enum(array('LT','RT'))->mandatory(true);
 		$this->addField('report_name')->enum(array('BalanceSheet','Trading','Profit & Loss'))->mandatory(true);
 		$this->addField('show_sub')->enum(array('SchemeGroup','SchemeName','Accounts'))->mandatory(true);
-		$this->addField('subtract_from')->enum(array('DR'))->mandatory(true);
+		$this->addField('subtract_from')->enum(array('DR','CR'))->mandatory(true);
 		$this->addField('order');
 		$this->addField('created_at')->type('date')->defaultValue($this->app->today);
 
