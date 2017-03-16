5f562f7186f1a0b3574a2e411c5e9a72a4a8d6be
vaibhavsharma38 <vaibhavsharma38@gmail.com>
2017-03-16 12:53:25 +0530

    save function implemented

--- page/transactionwidget.php
+++ page/transactionwidget.php
@@ -33,7 +33,45 @@ class page_transactionwidget extends \Page{
 
 	function page_save(){
 		$transaction_data = $_POST['transaction_data'];
-		$transaction_data = json_decode($transaction_data);
-		exit;
+		$transaction_data = json_decode($transaction_data,true);
+		
+		// echo"<pre>";
+		// print_r($transaction_data);
+		// echo "</pre>";
+		// exit;
+
+        $transactions=[];
+        $total_amount=[];
+        $related_transaction_id = null;
+
+        foreach ($transaction_data as $transaction) {
+            $transactions[] = $new_transaction = $this->add('xepan\accounts\Model_Transaction');
+            $new_transaction->createNewTransaction($transaction['type'],null,$transaction['date'],$transaction['narration'],$transaction['currency'],$transaction['exchange_rate'],null,null,$transaction['entry_template_transaction_id'], $transaction['entry_template_transaction_id']);
+            $total_amount[$transaction['type']] = 0;
+            
+            if($transaction['editing_transaction_id']){
+        		$transaction_row_m = $this->add('xepan\accounts\Model_TransactionRow');
+        		$transaction_row_m->addCondition('transaction_id',$transaction['editing_transaction_id']);
+        		$transaction_row_m->deleteAll();
+        	}
+        	
+            foreach ($transaction['rows'] as $code => $row) {
+            	if($row['currency'] === 'undefined' OR $row['currency'] == null OR $row['currency'] == 0)
+            		$currency_id = $this->app->epan->default_currency;
+            	
+            	if($row['exchange_rate'] === 'undefined' OR $row['exchange_rate'] == null OR $row['exchange_rate'] == 0)
+            		$exchange_rate = 1.00;
+
+                if(strtolower($row['data-side'])=='dr'){
+                    $new_transaction->addDebitLedger($row['data-ledger'],$row['data-amount'],$currency_id,$exchange_rate,$remark=null,$code);
+                    $total_amount[$transaction['type']] += $row['data-amount']* $exchange_rate;
+                }else{
+                    $new_transaction->addCreditLedger($row['data-ledger'],$row['data-amount'],$currency_id,$exchange_rate,$remark=null,$code);
+                }
+            }
+
+            if($total_amount[$transaction['type']] > 0)
+                $new_transaction->execute();
+        }               
 	}
 }
