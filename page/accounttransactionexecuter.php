05519f85bbb47520cbeb4369ad3f5fa49969954b
sinharakesh <rksinha.btech@gmail.com>
2017-03-18 11:23:42 +0530

    hardcode load of transaction line removed

--- page/accounttransactionexecuter.php
+++ page/accounttransactionexecuter.php
@@ -18,7 +18,7 @@ class page_accounttransactionexecuter extends \xepan\base\Page{
 
 		// $model->manageForm($this);
 		
-		// $model = $this->add('xepan\accounts\Model_Transaction')->load(813);
+		// $model = $this->add('xepan\accounts\Model_Transaction')->load(829);
 		$widget = $this->add('xepan\accounts\View_TransactionWidget');
 		$widget->setModel($model);
 	}
