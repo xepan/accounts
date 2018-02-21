06db1388eb744eeb2ee7c1bd16c88f965d415567
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-02-21 16:29:21 +0530

    show balance of ledgers and groups besides

--- page/chartofaccount.php
+++ page/chartofaccount.php
@@ -13,16 +13,24 @@ class page_chartofaccount extends \xepan\base\Page{
 		$obj = [];
 
 		foreach ($balance_model as $bm) {
-			$data = ['name' => $bm['name']];
-			foreach ($bm->ref('xepan\accounts\Group')->addCondition('parent_group_id',null) as $root_groups) {
-				$group_data=['name'=>'<b>'.$root_groups['name'].'</b>'];
+			$data = [];
+			$rg= $this->add('xepan\accounts\Model_Group')->addCondition('balance_sheet_id',$bm->id);
+			$head_sum = 0;
+			foreach ($rg->addCondition('parent_group_id',null) as $root_groups) {
+				$gb = $root_groups->getBalance();
+				$head_sum += $gb;
+				$gb = ABS($gb) . ($gb>0 ? ' Dr':' Cr');
+				$group_data=['name'=>'<b>'.$root_groups['name'].' => '.$gb.'</b>'];
 				$group_data['groups'] = $this->getChildGroups($root_groups);
 				$group_data['ledgers']=[];
 				foreach ($root_groups->ref('xepan\accounts\Ledger') as $ledger) {
-					$group_data['ledgers'][]= ['name'=>'<i>'.$ledger['name'].'</i>'];
+					$group_data['ledgers'][]= ['name'=>'<i>'.$ledger['name'].' => '.$ledger['balance'].'</i>'];
 				}
 				$data['groups'][] = $group_data;
 			}
+
+			$head_sum = ABS($head_sum) . ($head_sum>0 ? ' Dr':' Cr');
+			$data['name']='<span class="label label-primary">'.$bm['name'].' => '. $head_sum .'</span>';
 			$obj[] = $data;
 		}
 
@@ -37,11 +45,13 @@ class page_chartofaccount extends \xepan\base\Page{
 	function getChildGroups($group){
 		$data= [];
 		foreach ($this->add('xepan\accounts\Model_Group')->addCondition('parent_group_id',$group->id) as $sg) {
-			$t = ['name'=>'<b>'.$sg['name'].'</b>'];
+			$gb = $sg->getBalance();
+			$gb = ABS($gb) . ($gb>0 ? ' Dr':' Cr');
+			$t = ['name'=>'<b>'.$sg['name'].' => '.$gb.'</b>'];
 			$t['groups'] = $this->getChildGroups($sg);
 			$t['ledgers']=[];
 			foreach ($sg->ref('xepan\accounts\Ledger') as $ledger) {
-				$t['ledgers'][]= ['name'=>'<i>'.$ledger['name'].'</i>'];
+				$t['ledgers'][]= ['name'=>'<i>'.$ledger['name'].' => '.$ledger['balance'].'</i>'];
 			}
 			$data[] = $t;
 		}
